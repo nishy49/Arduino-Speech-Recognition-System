b4fe7f50af2481c1e0985a436494f2f88caccfc1
Nischal Prasad Raman <76789387+nishy49@users.noreply.github.com>
2021-12-11 13:01:46 +0530

    Add files via upload

---
+++ VoiceControlMain.java
@@ -0,0 +1,71 @@
+packagerobotspace.simplelabs.amr_voice;
+
+/* renamed from: robotspace.simplelabs.amr_voice.R */ publicfinalclass C0045R {
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$attr */ publicstaticfinalclassattr {
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$drawable */ publicstaticfinalclass drawable {
+publicstaticfinalint accelerometer = 2130837504; publicstaticfinalintbluetooth = 2130837505;
+publicstaticfinalintic_launcher = 2130837506;
+publicstaticfinalintminidroid = 2130837507;
+
+/* renamed from: robotspace reason: collision with root package name */ publicstaticfinalint f4robotspace = 2130837508; publicstaticfinalintsimplelabs = 2130837509;
+publicstaticfinalintsiri = 2130837510; publicstaticfinalint splash = 2130837511; publicstaticfinalintsplashscreen = 2130837512; publicstaticfinalint voice = 2130837513;
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$id */ publicstaticfinalclass C0046id { publicstaticfinalintBluetoothSettings = 2131165205; publicstaticfinalint Scan = 2131165204; publicstaticfinalintapp_info = 2131165206;
+publicstaticfinalintbutton_scan = 2131165203; publicstaticfinalint edittext1 = 2131165195; publicstaticfinalint edittext2 = 2131165197; publicstaticfinalint imageView1 = 2131165191; publicstaticfinalint imageView2 = 2131165192; publicstaticfinalintnew_devices = 2131165202;
+
+ 
+publicstaticfinalintpaired_devices = 2131165200; publicstaticfinalint save_mem1 = 2131165196; publicstaticfinalint save_mem2 = 2131165198; publicstaticfinalint savedmem1 = 2131165193; publicstaticfinalint savedmem2 = 2131165194; publicstaticfinalint textView1 = 2131165185; publicstaticfinalint textView2 = 2131165184; publicstaticfinalint textView3 = 2131165186; publicstaticfinalint textView4 = 2131165190; publicstaticfinalinttitle_new_devices = 2131165201;
+publicstaticfinalinttitle_paired_devices = 2131165199;
+publicstaticfinalintxview = 2131165187;
+publicstaticfinalintyview = 2131165188;
+publicstaticfinalintzview = 2131165189;
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$layout */ publicstaticfinalclass layout { publicstaticfinalintactivity_accelerometer_control = 2130903040;
+publicstaticfinalintactivity_app_info = 2130903041;
+publicstaticfinalintactivity_main = 2130903042;
+publicstaticfinalintactivity_splash_screen = 2130903043;
+publicstaticfinalintdevice_list = 2130903044;
+publicstaticfinalintdevice_name = 2130903045; publicstaticfinalint voice = 2130903046;
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$menu */ publicstaticfinalclass menu { publicstaticfinalintactivity_splash_screen = 2131099648;
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$string */ publicstaticfinalclass string {
+publicstaticfinalintBtSettings = 2130968580; publicstaticfinalint Scan = 2130968579; publicstaticfinalintapp_info = 2130968594;
+publicstaticfinalintapp_name = 2130968576;
+publicstaticfinalintbt_preference = 2130968590;
+publicstaticfinalintbutton_scan = 2130968587;
+
+ 
+publicstaticfinalintbutton_setdefault = 2130968592;
+publicstaticfinalintconnection_preference = 2130968591;
+publicstaticfinalintdefault_device = 2130968589;
+publicstaticfinalinthello_world = 2130968577;
+publicstaticfinalintmenu_settings = 2130968578;
+publicstaticfinalintnone_found = 2130968584;
+publicstaticfinalintnone_paired = 2130968583;
+publicstaticfinalintpreferences_activity = 2130968588; publicstaticfinalint scanning = 2130968581; publicstaticfinalintscreen_orientation = 2130968593;
+publicstaticfinalintselect_device = 2130968582;
+publicstaticfinalinttitle_other_devices = 2130968586;
+publicstaticfinalinttitle_paired_devices = 2130968585;
+
+/* renamed from: x */ publicstaticfinalint f2x = 2130968598;
+publicstaticfinalintx_axis = 2130968595;
+publicstaticfinalinty_axis = 2130968596;
+publicstaticfinalintz_axis = 2130968597;
+}
+
+/* renamed from: robotspace.simplelabs.amr_voice.R$style */ publicstaticfinalclass style { publicstaticfinalintAppBaseTheme = 2131034112;
+publicstaticfinalintAppTheme = 2131034113;
+}
+}
+
+
+
+
